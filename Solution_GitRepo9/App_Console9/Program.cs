17b560f279bdfa0b712e624fac289b1215cd0808
studenti <studenti@LAB2-PC-18>
2018-01-15 09:56:22 +0100

    Ho fatto una modifica alla solution

---
+++ Solution_GitRepo9/App_Console9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Console9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ciaoooo");
+            Console.WriteLine("Ho effettuato una modifica");
+            Console.ReadLine();
+        }
+    }
+}
